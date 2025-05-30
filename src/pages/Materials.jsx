cdcbe0530fa130888a04420b99d1fa95541b9d64
ShadowConYT <iamajay6122@gmail.com>
2025-05-30 21:49:05 +0530

    refactor: improve code formatting and enhance error handling in Materials and Uploads components

--- src/pages/Materials.jsx
+++ src/pages/Materials.jsx
@@ -4,73 +4,88 @@ import axios from "axios";
 import { useNavigate } from "react-router";
 
 const ExcelUploader = () => {
-    const [data, setData] = useState([]);
-    const navigate = useNavigate();
+  const [data, setData] = useState([]);
+  const navigate = useNavigate();
 
-    const handleFileUpload = (event) => {
-        const file = event.target.files[0];
-        if (!file) return;
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
 
-        const reader = new FileReader();
-        reader.readAsBinaryString(file);
+    const reader = new FileReader();
+    reader.readAsBinaryString(file);
 
-        reader.onload = (e) => {
-            const binaryStr = e.target.result;
-            const workbook = XLSX.read(binaryStr, { type: "binary" });
+    reader.onload = (e) => {
+      const binaryStr = e.target.result;
+      const workbook = XLSX.read(binaryStr, { type: "binary" });
 
-            // Assuming the first sheet is the one to read
-            const sheetName = workbook.SheetNames[0];
-            const sheet = workbook.Sheets[sheetName];
+      // Assuming the first sheet is the one to read
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
 
-            // Convert to JSON
-            const parsedData = XLSX.utils.sheet_to_json(sheet);
-            setData(parsedData);
-        };
+      // Convert to JSON
+      const parsedData = XLSX.utils.sheet_to_json(sheet);
+      setData(parsedData);
     };
+  };
 
-    console.log(data == 0);
+  console.log(data == 0);
 
-    const handleUpload = async () => {
+  const handleUpload = async () => {
+    if (data.length == 0) {
+      alert("No data to upload!");
+      return;
+    }
 
-        if (data.length == 0) {
-            alert("No data to upload!");
-            return;
+    try {
+      const response = await axios.put(
+        "http://localhost:8000/api/v0/user-objects",
+        { object: { items: data } },
+        {
+          headers: { "Content-Type": "application/json" },
         }
+      );
+      console.log("Upload Success:", response.data);
+    } catch (error) {
+      console.error("Upload Error:", error);
+    }
+  };
 
-        try {
-            const response = await axios.put("http://localhost:8000/api/v0/user-objects", {"object":{"items": data}}, {
-                headers: { "Content-Type": "application/json" }
-            });
-            console.log("Upload Success:", response.data);
-        } catch (error) {
-            console.error("Upload Error:", error);
-        }
-    };
-
-    return (
-        <div className="border-2 border-black rounded-xl shadow-xl p-3 my-5">
-            <div className="text-xl font-bold pl-5">
-                <h1>Material Excel Uploader</h1>
-            </div>
-            <div className="flex justify-between p-5">
-                <input className="bg-gray-300 p-3 rounded-xl cursor-pointer" required type="file" accept=".xlsx, .xls" onChange={handleFileUpload} />
-                <div className="flex gap-x-5">
-                    <button 
-                        disabled={data.length === 0}
-                        className="bg-blue-600 px-3 py-2 text-white rounded-xl cursor-pointer hover:bg-blue-800 disabled:bg-gray-400 disabled:cursor-not-allowed" 
-                        onClick={handleUpload}>
-                            Upload
-                    </button>
-                    <button
-                        className="bg-blue-600 px-3 py-2 text-white rounded-xl cursor-pointer hover:bg-blue-800" 
-                        onClick={() => navigate('/material')} 
-                        >
-                        Check Materials
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className="border-2 border-black rounded-xl shadow-xl p-3 my-5">
+      <div className="text-xl font-bold pl-5">
+        <h1>Material Excel Uploader</h1>
+      </div>
+      <div className="flex justify-between p-5">
+        <input
+          className="block text-sm text-gray-500
+            file:mr-4 file:py-2 file:px-4
+            file:rounded-md file:border-0
+            file:text-sm file:font-medium
+            file:bg-blue-50 file:text-blue-700
+            hover:file:bg-blue-100"
+          required
+          type="file"
+          accept=".xlsx, .xls"
+          onChange={handleFileUpload}
+        />
+        <div className="grid grid-cols-2 gap-x-5">
+          <button
+            disabled={data.length === 0}
+            className="bg-blue-600 px-3 py-2 text-white rounded-xl cursor-pointer hover:bg-blue-800 disabled:bg-gray-400 disabled:cursor-not-allowed"
+            onClick={handleUpload}
+          >
+            Upload
+          </button>
+          <button
+            className="bg-blue-600 px-3 py-2 text-white rounded-xl cursor-pointer hover:bg-blue-800"
+            onClick={() => navigate("/material")}
+          >
+            Check Materials
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ExcelUploader;
--- src/pages/Uploads.jsx
+++ src/pages/Uploads.jsx
@@ -1,27 +1,32 @@
-import { useEffect, useState, useCallback, useMemo } from 'react';
-import { PlusCircle, X } from 'lucide-react';
-import * as XLSX from 'xlsx';
-import { useDispatch, useSelector } from 'react-redux';
-import { getShipmentDetails, loadShipmentDetails } from '../actions/shipmentAction';
-import { toast } from 'react-toastify';
-import { Button } from '@mui/material';
-import { clearShipmentErrors } from '../slices/shipmentSlice';
-import countryList from 'react-select-country-list';
-import { useNavigate } from 'react-router';
-import { logOutUser } from '../actions/userAction';
-import Materials from './Materials';
+import { useEffect, useState, useCallback, useMemo } from "react";
+import { PlusCircle, X } from "lucide-react";
+import * as XLSX from "xlsx";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getShipmentDetails,
+  loadShipmentDetails,
+} from "../actions/shipmentAction";
+import { toast } from "react-toastify";
+import { Button } from "@mui/material";
+import { clearShipmentErrors } from "../slices/shipmentSlice";
+import countryList from "react-select-country-list";
+import { useNavigate } from "react-router";
+import { logOutUser } from "../actions/userAction";
+import Materials from "./Materials";
 
 const ShipmentTracker = () => {
-  const [trackingNumbers, setTrackingNumbers] = useState([{
-    id: 1,
-    ref: '',
-    mode_of_transport: '',
-    plant: '',
-    liner_name: 'maersk',
-    origin_location: '',
-    invoice_date: ''
-  }]);
-  
+  const [trackingNumbers, setTrackingNumbers] = useState([
+    {
+      id: 1,
+      ref: "",
+      mode_of_transport: "",
+      plant: "",
+      liner_name: "maersk",
+      origin_location: "",
+      invoice_date: "",
+    },
+  ]);
+
   const [excelData, setExcelData] = useState(null);
   const [uploadedFile, setUploadedFile] = useState(null);
   const [listFormData, setListFormData] = useState([]);
@@ -30,33 +35,49 @@ const ShipmentTracker = () => {
 
   const changePage = (url) => {
     window.location.href = url;
-  }
+  };
 
-  const options = useMemo(() => countryList().getData(), []);
+  const options = [
+    "singapore",
+    "china",
+    "taiwan",
+    "denmark",
+    "hungary",
+    "mexico",
+    "serbia",
+    "italy",
+    "germany",
+    "south korea",
+    "usa",
+  ];
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { shipments, shipmentError, shipmentErrors, isDownloadEmailSent } = useSelector((state) => state.shipment);
+  const { shipments, shipmentError, shipmentErrors, isDownloadEmailSent } =
+    useSelector((state) => state.shipment);
   const { isAuthenticated } = useSelector((state) => state.auth);
 
   useEffect(() => {
     if (!isAuthenticated) {
-      navigate('/login');
+      navigate("/login");
     }
   }, [isAuthenticated]);
 
-  const carriers = useMemo(() => [
-    { value: 'maersk', label: 'Maersk' },
-    { value: 'dhl', label: 'DHL' },
-    { value: 'geodis', label: 'Geodis' }
-  ], []);
+  const carriers = useMemo(
+    () => [
+      { value: "maersk", label: "Maersk" },
+      { value: "dhl", label: "DHL" },
+      { value: "geodis", label: "Geodis" },
+    ],
+    []
+  );
 
   const handleFileUpload = useCallback(async (event) => {
     const file = event.target.files[0];
     if (!file) return;
 
-    if (!file.name.endsWith('.xlsx')) {
-      setError('Please upload a valid XLSX file');
-      toast.error('Please upload a valid XLSX file');
+    if (!file.name.endsWith(".xlsx")) {
+      setError("Please upload a valid XLSX file");
+      toast.error("Please upload a valid XLSX file");
       return;
     }
 
@@ -66,7 +87,7 @@ const ShipmentTracker = () => {
 
     reader.onload = (e) => {
       try {
-        const workBook = XLSX.read(e.target.result, { type: 'binary' });
+        const workBook = XLSX.read(e.target.result, { type: "binary" });
         const sheetName = workBook.SheetNames[0];
         const sheet = workBook.Sheets[sheetName];
         const sheetData = XLSX.utils.sheet_to_json(sheet);
@@ -74,18 +95,22 @@ const ShipmentTracker = () => {
         setExcelData(sheetData);
       } catch (err) {
         console.error("Error parsing excel file", err);
-        setError('Error parsing the Excel file. Please ensure it is correctly formatted.');
-        toast.error('Error parsing the Excel file. Please ensure it is correctly formatted.');
+        setError(
+          "Error parsing the Excel file. Please ensure it is correctly formatted."
+        );
+        toast.error(
+          "Error parsing the Excel file. Please ensure it is correctly formatted."
+        );
         setUploadedFile(null);
       }
     };
 
     reader.onerror = (err) => {
       console.error("Error reading file", err);
-      setError('Error reading the file.');
-      toast.error('Error reading the file.');
+      setError("Error reading the file.");
+      toast.error("Error reading the file.");
       setUploadedFile(null);
-    }
+    };
 
     reader.readAsArrayBuffer(file);
   }, []);
@@ -102,7 +127,7 @@ const ShipmentTracker = () => {
         origin_location: content["Origin Country"],
         mode_of_transport: content["MOT"].toLowerCase(),
         plant: content["Plant"],
-        invoice_date: content["Invoice Date"]
+        invoice_date: content["Invoice Date"],
       }));
       console.log(newApiFormData);
       setListFormData(newApiFormData);
@@ -124,7 +149,7 @@ const ShipmentTracker = () => {
 
   useEffect(() => {
     if (isDownloadEmailSent) {
-      toast.success('Email sent successfully');
+      toast.success("Email sent successfully");
       // dispatch(clearDownloadEmailSent());
     }
   }, [isDownloadEmailSent, dispatch]);
@@ -137,83 +162,88 @@ const ShipmentTracker = () => {
   }, []);
 
   const handleInputChange = useCallback((id, field, value) => {
-    setTrackingNumbers(prev =>
-      prev.map(item =>
-        item.id === id ? { ...item, [field]: value } : item
-      )
+    setTrackingNumbers((prev) =>
+      prev.map((item) => (item.id === id ? { ...item, [field]: value } : item))
     );
   }, []);
 
   const addTrackingNumber = useCallback(() => {
-    setTrackingNumbers(prev => [
+    setTrackingNumbers((prev) => [
       ...prev,
       {
-        id: prev.length > 0 ? Math.max(...prev.map(item => item.id)) + 1 : 1,
-        ref: '',
-        mode_of_transport: '',
-        plant: '',
-        liner_name: 'maersk',
-        origin_location: '',
-        invoice_date: ''
-      }
+        id: prev.length > 0 ? Math.max(...prev.map((item) => item.id)) + 1 : 1,
+        ref: "",
+        mode_of_transport: "",
+        plant: "",
+        liner_name: "maersk",
+        origin_location: "",
+        invoice_date: "",
+      },
     ]);
   }, []);
 
   const removeTrackingNumber = useCallback((id) => {
-    setTrackingNumbers(prev => prev.filter(item => item.id !== id));
+    setTrackingNumbers((prev) => prev.filter((item) => item.id !== id));
   }, []);
 
-  const trackShipments = useCallback(async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    setError(null);
-    try {
-      let formData;
+  const trackShipments = useCallback(
+    async (e) => {
+      e.preventDefault();
+      setLoading(true);
+      setError(null);
+      try {
+        let formData;
 
-      if (uploadedFile && listFormData.length > 0) {
-        formData = listFormData;
-      } else {
-        formData = trackingNumbers.map(item => ({
-          ref: item.ref,
-          liner_name: item.liner_name,
-          origin_location: item.origin_location,
-          mode_of_transport: item.mode_of_transport,
-          plant: item.plant,
-          invoice_date: item.invoice_date
-        }));
+        if (uploadedFile && listFormData.length > 0) {
+          formData = listFormData;
+        } else {
+          formData = trackingNumbers.map((item) => ({
+            ref: item.ref,
+            liner_name: item.liner_name,
+            origin_location: item.origin_location,
+            mode_of_transport: item.mode_of_transport,
+            plant: item.plant,
+            invoice_date: item.invoice_date,
+          }));
+        }
+
+        await dispatch(getShipmentDetails(formData));
+        await dispatch(loadShipmentDetails());
+      } catch (err) {
+        console.error("Tracking error:", err);
+        setError(err.message || "An error occurred while tracking shipments");
+        toast.error("An error occurred while tracking shipments");
+      } finally {
+        setLoading(false);
       }
-
-      await dispatch(getShipmentDetails(formData));
-      await dispatch(loadShipmentDetails());
-    } catch (err) {
-      console.error('Tracking error:', err);
-      setError(err.message || 'An error occurred while tracking shipments');
-      toast.error('An error occurred while tracking shipments');
-    } finally {
-      setLoading(false);
-    }
-  }, [uploadedFile, listFormData, trackingNumbers, dispatch]);
+    },
+    [uploadedFile, listFormData, trackingNumbers, dispatch]
+  );
 
   const logOutHandler = () => {
     dispatch(logOutUser());
-  }
+  };
 
-  console.log(trackingNumbers)
+  console.log(trackingNumbers);
 
   return (
     <div className="min-h-screen bg-gray-50 pb-4 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-7xl mx-auto"> 
-        <div className='flex justify-between items-center mb-4'>
+      <div className="max-w-7xl mx-auto">
+        <div className="flex justify-between items-center mb-4">
           <div className="text-left mb-1">
-            <h1 className="text-3xl font-bold text-gray-900 mb-2">Shipment Tracker</h1>
-            <p className="text-gray-600">Track multiple shipments across different carriers</p>
+            <h1 className="text-3xl font-bold text-gray-900 mb-2">
+              Shipment Tracker
+            </h1>
+            <p className="text-gray-600">
+              Track multiple shipments across different carriers
+            </p>
           </div>
         </div>
         <Materials />
-        <div className='border-2 border-black p-3 rounded-xl'>
+        <div className="border-2 border-black p-3 rounded-xl">
           <div className="bg-white rounded-lg shadow-sm p-6 mb-4">
             <div>
-              <h1 className='font-bold text-xl mb-4'>HBL Tracker</h1>
+              <h1 className="font-bold text-xl mb-4">HBL Tracker</h1>
             </div>
             <div className="mb-6">
               <div className="flex items-center gap-4">
@@ -250,19 +280,33 @@ const ShipmentTracker = () => {
               {trackingNumbers.map((item) => (
                 <div key={item.id} className="flex gap-4 items-center">
                   <div className="flex-1">
+                    <label className="block text-sm font-medium text-gray-700 mb-2">
+                      Tracking Number
+                    </label>
                     <input
                       type="text"
                       placeholder="Enter tracking number"
                       value={item.ref}
-                      onChange={(e) => handleInputChange(item.id, 'ref', e.target.value)}
+                      onChange={(e) =>
+                        handleInputChange(item.id, "ref", e.target.value)
+                      }
                       className="w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-500 disabled:bg-gray-100 disabled:cursor-not-allowed"
                       disabled={!!uploadedFile}
                     />
                   </div>
                   <div>
+                    <label className="block text-sm font-medium text-gray-700 mb-2">
+                      Select Mode of Transport
+                    </label>
                     <select
                       value={item.mode_of_transport}
-                      onChange={(e) => handleInputChange(item.id, 'mode_of_transport', e.target.value)}
+                      onChange={(e) =>
+                        handleInputChange(
+                          item.id,
+                          "mode_of_transport",
+                          e.target.value
+                        )
+                      }
                       className="w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-500 disabled:bg-gray-100 disabled:cursor-not-allowed"
                       disabled={!!uploadedFile}
                     >
@@ -271,35 +315,61 @@ const ShipmentTracker = () => {
                       <option value="air">Air</option>
                     </select>
                   </div>
-                  <div className='w-48'>
-                    <input
+                  <div className="w-48">
+                    <label className="block text-sm font-medium text-gray-700 mb-2">
+                      Select Plant
+                    </label>
+                    <select
+                      value={item.plant}
                       type="text"
                       placeholder="Enter Plant"
-                      value={item.plant}
-                      onChange={(e) => handleInputChange(item.id, 'plant', e.target.value)}
-                      className="w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-500 disabled:bg-gray-100 disabled:cursor-not-allowed"
-                      disabled={!!uploadedFile}
-                    />
-                  </div>
-                  <div className='w-48'>
-                    <select
-                      value={item.origin_location}
-                      onChange={(e) => handleInputChange(item.id, 'origin_location', e.target.value)}
+                      onChange={(e) =>
+                        handleInputChange(item.id, "plant", e.target.value)
+                      }
                       className="w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-500 disabled:bg-gray-100 disabled:cursor-not-allowed"
                       disabled={!!uploadedFile}
                     >
-                      <option value="">Select Origin Location</option>
-                      {options.map((option) => (
-                        <option key={option.value} value={option.label}>
-                          {option.label}
+                      <option value="">Select Plant</option>
+                      {["GIN1", "GIN9"].map((plant) => (
+                        <option key={plant} value={plant}>
+                          {plant}
                         </option>
                       ))}
                     </select>
                   </div>
                   <div className="w-48">
+                    <label className="block text-sm font-medium text-gray-700 mb-2">
+                      Select Origin Location
+                    </label>
+                    <select
+                      value={item.origin_location}
+                      onChange={(e) =>
+                        handleInputChange(
+                          item.id,
+                          "origin_location",
+                          e.target.value
+                        )
+                      }
+                      className="w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-500 disabled:bg-gray-100 disabled:cursor-not-allowed"
+                      disabled={!!uploadedFile}
+                    >
+                      <option value="">Select Origin Location</option>
+                      {options.map((option) => (
+                        <option key={option} value={option}>
+                          {option}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <div className="w-48">
+                    <label className="block text-sm font-medium text-gray-700 mb-2">
+                      Select Liner Name
+                    </label>
                     <select
                       value={item.liner_name}
-                      onChange={(e) => handleInputChange(item.id, 'liner_name', e.target.value)}
+                      onChange={(e) =>
+                        handleInputChange(item.id, "liner_name", e.target.value)
+                      }
                       className="w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-500 disabled:bg-gray-100 disabled:cursor-not-allowed"
                       disabled={!!uploadedFile}
                     >
@@ -310,12 +380,21 @@ const ShipmentTracker = () => {
                       ))}
                     </select>
                   </div>
-                  <div className='w-48'>
+                  <div className="w-48">
+                    <label className="block text-sm font-medium text-gray-700 mb-2">
+                      Invoice Date
+                    </label>
                     <input
                       type="date"
                       placeholder="Enter Invoice Date"
                       value={item.invoice_date}
-                      onChange={(e) => handleInputChange(item.id, 'invoice_date', e.target.value)}
+                      onChange={(e) =>
+                        handleInputChange(
+                          item.id,
+                          "invoice_date",
+                          e.target.value
+                        )
+                      }
                       className="w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-500 disabled:bg-gray-100 disabled:cursor-not-allowed"
                       disabled={!!uploadedFile}
                     />
@@ -332,10 +411,13 @@ const ShipmentTracker = () => {
               ))}
             </div>
 
-            <div className='p-5 float-right'>
-              <Button className='!bg-blue-600 !text-white !font-normal hover:!bg-blue-700' onClick={trackShipments}>
-                  Submit
-              </Button> 
+            <div className="p-5 float-right">
+              <Button
+                className="!bg-blue-600 !text-white !font-normal hover:!bg-blue-700"
+                onClick={trackShipments}
+              >
+                Submit
+              </Button>
             </div>
 
             {!uploadedFile && (
@@ -351,8 +433,11 @@ const ShipmentTracker = () => {
             )}
           </div>
 
-          <div className='w-full flex justify-center'>
-            <Button onClick={() => navigate('/live')} className='!w-48 !bg-blue-600 !text-white !p-2 !rounded-xl'>
+          <div className="w-full flex justify-center">
+            <Button
+              onClick={() => navigate("/live")}
+              className="!w-48 !bg-blue-600 !text-white !p-2 !rounded-xl"
+            >
               Check Live Status
             </Button>
             {/* <button onClick={() => logOutHandler()} className='w-48 bg-red-600 text-white  p-2 rounded-xl'>
